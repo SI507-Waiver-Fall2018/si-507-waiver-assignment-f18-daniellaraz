034f16b6274cd9e49e680a71fd603c3c9ff27df3
Daniella Raz <daniellaraz@gmail.com>
2018-06-21 13:00:56 +0300

    Finished part2.py

--- part2.py
+++ part2.py
@@ -1,4 +1,4 @@
-## Full Name: Daniella R. Raz
+## Full Name: Daniella Raz
 ## Uniqname: drraz
 ## UMID: 86870313
 
@@ -21,14 +21,16 @@ if len(argument_list)>1:
     column_info = argument_list[1]
     col_name, col_value = column_info.split("=")
 
+# connecting to database
 conn = sqlite3.connect('Northwind_small.sqlite')
 cur = conn.cursor()
 
+# sqlite queries, and formatting to print out like in the sample output
 if table_name == 'Customer':
     customer_names = cur.execute("SELECT Id, CompanyName FROM %s" % (table_name))
     customer_names = customer_names.fetchall()
     print("ID    Customer Name")
-    for customer_id, customer_name in customer_names:  # <-- this unpacks the tuple like a, b = (0, 1)
+    for customer_id, customer_name in customer_names:
         print(customer_id, customer_name)
 
 elif table_name == 'Employee':
@@ -38,18 +40,20 @@ elif table_name == 'Employee':
     for employee_id, first_name, last_name in employee_names:
         print(employee_id, "  ", first_name, last_name)
 
-
 else:
     if col_name == 'cust':
-        order_date_from_customer = cur.execute("""SELECT OrderDate FROM [Order] WHERE CustomerId = ?;""", (col_value,))
+        order_date_from_customer = cur.execute("SELECT OrderDate FROM [Order] WHERE CustomerId = ?;", (col_value,))
         order_date_from_customer = order_date_from_customer.fetchall()
         print("Order dates")
         for date_tuple in order_date_from_customer:
             for date in date_tuple:
                 print(date)
     else:
-        order_dates_from_employee = cur.execute('SELECT OrderDate FROM [Order] WHERE EmployeeID = (SELECT Id FROM Employee WHERE LastName = %s)' %(col_value))
+        order_dates_from_employee = cur.execute("SELECT OrderDate FROM [Order] WHERE EmployeeID = (SELECT Id from Employee WHERE LastName = ?)", (col_value,))
         order_dates_from_employee = order_dates_from_employee.fetchall()
-        print(order_dates_from_employee)
+        print("Order dates")
+        for dates_in_tuple in order_dates_from_employee:
+            for date in dates_in_tuple:
+                print(date)
 
 conn.close()
